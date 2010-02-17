aa91146eedb6e143dbc8df75572c427a5ed33181
Jeffrey ODell <jeffrey.odell@gmail.com>
2010-02-17 10:39:41 -0600

    master: add output file option to cli, test

--- test/tc_cli.rb
+++ test/tc_cli.rb
@@ -2,10 +2,14 @@
 require 'toycipher'
 require 'test/unit'
 
-class TestCLI < Test::Unit::TestCase
+class TestCli < Test::Unit::TestCase
 
   def setup
-    @cli = File.expand_path(File.dirname(__FILE__)) + '/../script/toycipher'
+    @cli ||= File.expand_path(File.dirname(__FILE__)) + '/../script/toycipher'
+    @pt ||= 'Hide the gold in the tree stump'
+    @k ||= 'playfair example'
+    @pf_ciphertext ||= 'BM ND ZB XD KY BE JV DM UI XM MN UV IF '
+    @outfile ||= '/tmp/test_toycipher_cli.out'
   end
 
   def teardown
@@ -27,16 +31,21 @@ class TestCLI < Test::Unit::TestCase
   end
 
   def test_cli_playfair
-    pt = 'Hide the gold in the tree stump'
-    k = 'playfair example'
-    args = "-e '#{pt}' -c playfair -k '#{k}'"
+    args = "-e '#{@pt}' -c playfair -k '#{@k}'"
     cli_out = %x[#{@cli} #{args}].chomp
-    assert_equal 'BM ND ZB XD KY BE JV DM UI XM MN UV IF ', cli_out 
-    assert_equal ToyCipher::Playfair.new.encrypt(pt, k), cli_out
+    assert_equal @pf_ciphertext, cli_out 
+    assert_equal ToyCipher::Playfair.new.encrypt(@pt, @k), cli_out
     args =  %Q(-d "FDSDDSSS" -c playfair -k "EFF")
-
     cli_out = %x[#{@cli} #{args}].chomp.strip
     assert_equal true, !!cli_out.match(/could not decrypt/i)
+  end
+
+  def test_cli_out_file
+    args = "-e '#{@pt}' -c playfair -k '#{@k}' -o #{@outfile}"
+    cli_out = %x[#{@cli} #{args}].chomp
+    assert_equal '', cli_out
+    assert_equal true, File.exists?(@outfile)
+    assert_equal @pf_ciphertext, IO.readlines(@outfile).first.chomp
   end
 
 end
