dc9a212d40587fa228e1ab3da14651616f6295d6
Jeffrey ODell <jeffrey.odell@gmail.com>
2010-02-16 21:55:03 -0600

    master: rename scripts dir, add autotest (yay\!), test mods

--- test/tc_cli.rb
+++ test/tc_cli.rb
@@ -5,7 +5,7 @@ require 'test/unit'
 class TestCLI < Test::Unit::TestCase
 
   def setup
-    @cli = File.expand_path(File.dirname(__FILE__)) + '/../scripts/toycipher'
+    @cli = File.expand_path(File.dirname(__FILE__)) + '/../script/toycipher'
   end
 
   def teardown
@@ -30,9 +30,9 @@ class TestCLI < Test::Unit::TestCase
     pt = 'Hide the gold in the tree stump'
     k = 'playfair example'
     args = "-e '#{pt}' -c playfair -k '#{k}'"
-    cli_out = %x[#{@cli} #{args}].chomp.strip
-    assert_equal cli_out, 'BM ND ZB XD KY BE JV DM UI XM MN UV IF'
-    assert_equal ToyCipher::Playfair.new.encrypt(pt, k).strip, cli_out
+    cli_out = %x[#{@cli} #{args}].chomp
+    assert_equal 'BM ND ZB XD KY BE JV DM UI XM MN UV IF ', cli_out 
+    assert_equal ToyCipher::Playfair.new.encrypt(pt, k), cli_out
     args =  %Q(-d "FDSDDSSS" -c playfair -k "EFF")
 
     cli_out = %x[#{@cli} #{args}].chomp.strip
