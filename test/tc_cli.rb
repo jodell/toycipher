d3ad4d82871ee20efe56df44301f59897b739d2d
Jeffrey ODell <jeffrey.odell@gmail.com>
2010-03-25 10:13:01 -0500

    master: readme, roadmap, gitignore, test & script updates

--- test/tc_cli.rb
+++ test/tc_cli.rb
@@ -5,6 +5,27 @@ require 'fileutils'
 
 class TestCli < Test::Unit::TestCase
 
+  SHMOO_ANS_1 = <<-SHMOO
+OYETLZZZZBNZTTZRUWTPAHXOANNOEEAZCRHEZYZIZATEZZZHTTZZCIEEASDTZSLEKQALKRADYIODBLMY
+KOZTEZOHYRZCOZFETZROLETZTETUSZNTAZEYEMBZTKZWEZYAOZRZHNMRTADRDOTCFMELMYQCLDQHTRCY
+AUPYVNNAOOTRZYIDZTASLZSCEZISSZTHECRZPOUHOESHANOVZTENEGEZZIOZRYRWLRALEELDQHTRCYET
+YZRZEOLVUKHYBOGZHONEZTZRZCNZAGEESIZIOUTAZZOASOUEGHSOAZMWIDLSIYZHLBZSALEECGQDCAIY
+ZRECRTYEZEEPUUUOOZSZTETEOOUMGRDZAPKSNSZDMIMTYWZZEETZTRBHZZLTSQATTSQOSTSOZHVYPODL
+SHMOO
+
+  SHMOO_CT = <<-SHMOO
+VFLASGGGGIUGAAGYBDAWHOEVHUUVLLHGJYOLGFGPGHALGGGOAAGGJPLLHZKAGZSLRXHSRYHKFPVKISTF
+XBMGRMBULEMPBMSRGMEBYRGMGRGHFMAGNMRLRZOMGXMJRMLNBMEMUAZEGNQEQBGPSZRYZLDPYQDUGEPL
+BVQZWOOBPPUSAZJEAUBTMATDFAJTTAUIFDSAQPVIPFTIBOPWAUFOFHFAAJPASZSXMSBMFFMERIUSDZFU
+QRJRWGDNMCZQTGYRZGFWRLRJRUFRSYWWKARAGMLSRRGSKGMWYZKGSREOAVDKAQRZDTRKSDWWUYIVUSAQ
+KCPNCEJPKPPAFFFZZKDKEPEPZZFXRCOKLAVDYDKOXTXEJHKKPPEKECMSKKWEDBLEEDBZDEDZKSGJAZOW
+SHMOO
+
+  TF = {
+    :out_f => '/tmp/test_toycipher_cli.out',
+    :shmoocon2010_1 => '/tmp/shmoocon2010_1'
+  }.freeze
+
   def setup
     @cli ||= File.expand_path(File.dirname(__FILE__)) + '/../script/toycipher'
     @pt ||= 'Hide the gold in the tree stump'
@@ -12,11 +33,10 @@ class TestCli < Test::Unit::TestCase
     @k2 ||= 'foo bar'
     @pf_ciphertext  ||= 'BM ND ZB XD KY BE JV DM UI XM MN UV IF '
     @pf_ciphertext2 ||= 'CM EG UG GH AJ CJ PU CG UA KB KX UN JU '
-    @outfile ||= '/tmp/test_toycipher_cli.out'
   end
 
   def teardown
-    FileUtils.rm @outfile if File.exists? @outfile
+    TF.each { |k, f| FileUtils.rm f if File.exists?(f) }
   end
 
   def test_cli_help
@@ -53,11 +73,11 @@ class TestCli < Test::Unit::TestCase
   end
 
   def test_cli_out_file
-    args = "-e '#{@pt}' -c playfair -k '#{@k}' -o #{@outfile}"
-    puts "Running '#{@clie} #{args}'"
+    args = "-e '#{@pt}' -c playfair -k '#{@k}' -o #{TF[:out_f]}"
+    #puts "Running '#{@clie} #{args}'"
     cli_out = %x[#{@cli} #{args}].chomp
     assert_equal '', cli_out
-    assert_equal true, File.exists?(@outfile)
+    assert_equal true, File.exists?(TF[:out_f])
     #assert_equal @pf_ciphertext, IO.readlines(@outfile).first.chomp
   end
 
@@ -66,10 +86,16 @@ class TestCli < Test::Unit::TestCase
 
   def test_multi_keys
     args = "-e '#{@pt}' -c playfair -k '#{@k},#{@k2}' "
-    puts "Running '#{@clie} #{args}'"
+    #puts "Running '#{@clie} #{args}'"
     cli_out = %x[#{@cli} #{args}].chomp
     #puts cli_out
     assert_equal [@pf_ciphertext, @pf_ciphertext2].join("\n"), cli_out
+  end
+
+  def test_shmoocon2010_part1
+    shmooct = File.open(TF[:shmoocon2010_1], 'w') { |f| f << SHMOO_CT }
+    args = "-e -i #{TF[:shmoocon2010_1]} -c caesar -k 'G'"
+    cli_out = %x[#{@cli} #{args}].chomp
   end
 
 end
