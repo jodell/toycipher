5528b2b5b2c2655df64c29c80c5b3c25287744e1
Jeffrey ODell <jeffrey.odell@gmail.com>
2010-07-30 17:32:26 -0700

    unit test, round 3 raw

--- test/tc_morse.rb
+++ test/tc_morse.rb
@@ -11,6 +11,8 @@ class TestMorse < Test::Unit::TestCase
 
   def setup
     @cipher = ToyCipher::Morse.new
+    @mc2010_ciphetext = '.... .- ...- . ..-. ..- -. .--. .- ... ... .-- --- .-. -.. --... ..... ..... --... .... . .-.. .-.. --- .... .- ...- . ..-. ..- -.'
+    @mc2010_plaintext = 'HAVEFUNPASSWORD7557HELLOHAVEFUN'
   end
 
   def teardown
@@ -19,6 +21,11 @@ class TestMorse < Test::Unit::TestCase
   def test_morse
     assert @cipher.is_a?(ToyCipher::Morse)
   end
+
+  def test_cli
+    assert_equal @mc2010_plaintext, @cipher.decrypt(@mc2010_ciphetext)
+  end
+
 
 
 
